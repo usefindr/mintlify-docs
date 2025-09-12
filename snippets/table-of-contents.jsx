034b73c519089e457da9249de038eb1bbe3a279b
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2025-09-12 09:32:26 +0530

    feat: improve sidebar page alignment

--- snippets/table-of-contents.jsx
+++ snippets/table-of-contents.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export const TableOfContents = ({
   title = 'On this page',
@@ -110,16 +110,18 @@ export const TableOfContents = ({
   const data = toc && toc.length ? toc : items || []
 
   return (
-    <div className={`text-gray-600 text-sm leading-6 w-[25rem] space-y-2 pb-4 -mt-10 pt-10 hidden lg:block ${className}`} id="table-of-contents">
-      <div className="text-gray-700 dark:text-gray-300 font-medium flex items-center space-x-2">
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" stroke="currentColor" strokeWidth="2" xmlns="http://www.w3.org/2000/svg" className="h-3 w-3">
-          <path d="M2.44434 12.6665H13.5554" strokeLinecap="round" strokeLinejoin="round"></path>
-          <path d="M2.44434 3.3335H13.5554" strokeLinecap="round" strokeLinejoin="round"></path>
-          <path d="M2.44434 8H7.33323" strokeLinecap="round" strokeLinejoin="round"></path>
-        </svg>
-        <span>{title}</span>
-      </div>
+    <div className={`text-gray-600 text-sm leading-6 w-[22rem] pb-4 -mt-10 pt-10 hidden lg:block ${className}`} id="table-of-contents">
       <ul id="table-of-contents-content" className="toc">
+        <li className="toc-item relative">
+          <div className="text-gray-700 dark:text-gray-300 font-medium flex items-center space-x-2 py-1">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" stroke="currentColor" strokeWidth="2" xmlns="http://www.w3.org/2000/svg" className="h-3 w-3">
+              <path d="M2.44434 12.6665H13.5554" strokeLinecap="round" strokeLinejoin="round"></path>
+              <path d="M2.44434 3.3335H13.5554" strokeLinecap="round" strokeLinejoin="round"></path>
+              <path d="M2.44434 8H7.33323" strokeLinecap="round" strokeLinejoin="round"></path>
+            </svg>
+            <span>{title}</span>
+          </div>
+        </li>
         {data.map((node) => (
           <Item node={node} key={node.href} />
         ))}
